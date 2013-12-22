00227e507ec5242e1d2ef78735c0ab5f7a63efc1
ShawnDriscoll <shawndriscoll@hotmail.com>
2013-12-22 10:35:49 -0800

    Fixed tabs

--- diceroll.h
+++ diceroll.h
@@ -7,34 +7,34 @@
 //
 
 int die_roll(int die)
-	{ 
-    return random(die) + 1;
-	}
+{
+	return random(die) + 1;
+}
 
 int roll(char dice[10])
-	{
-		int value;
-		int i;		
+{
+	int value;
+	int i;		
 		
     if (dice[0] == 68)   // "D" is at beginning of roll string?
-    	{
-    		printf("D00 and D10 code goes here!\n");
-    		return 0;
-    	}
-    else
-    	{
-    		if (dice[1] == 68)  // "D" is in middle of roll string?
-    			{
-    				if (dice[0] >= 48 && dice[0] <= 57)
-    					{
-    						value = 0;
-    						for (i = 1; i < dice[0] - 47; i++)  // Roll the number of dice
-    							value += die_roll(dice[2] - 48);  // Add this die roll type to the total
-    						return value;
-    					}
-					}
-			}
-		printf("DICE ERROR!\n");
-		printf("%s unknown\n", dice);
+	{
+		printf("D00 and D10 code goes here!\n");
 		return 0;
 	}
+    else
+	{
+		if (dice[1] == 68)  // "D" is in middle of roll string?
+		{
+			if (dice[0] >= 48 && dice[0] <= 57)
+			{
+				value = 0;
+				for (i = 1; i < dice[0] - 47; i++)  // Roll the number of dice
+					value += die_roll(dice[2] - 48);  // Add this die roll type to the total
+				return value;
+			}
+		}
+	}
+	printf("DICE ERROR!\n");
+	printf("%s unknown\n", dice);
+	return 0;
+}
--- solomani.c
+++ solomani.c
@@ -45,9 +45,9 @@ int CVC = 4;
 int CC  = 5;
 
 char ic_sound[45][3] = {"b","bl","br","c","ch","cl","cr","d","dr","f","fl","fr","g","gl",
-                "gr","h","j","k","kl","l","m","n","p","ph","pl","pr","qu",
-                "r","s","sc","sh","sl","sk","st","sv",
-                "t","th","tr","tw","v","w","wr","x","z","zh"};
+						"gr","h","j","k","kl","l","m","n","p","ph","pl","pr","qu",
+						"r","s","sc","sh","sl","sk","st","sv",
+						"t","th","tr","tw","v","w","wr","x","z","zh"};
 int ic_freq[45] = {10,3,5,7,6,5,6,10,8,7,3,4,4,3,4,7,7,5,4,10,9,9,10,5,
                2,5,2,9,11,2,9,10,7,9,2,7,6,5,5,3,3,4,1,3,1};
     
@@ -58,16 +58,16 @@ char mc_sound[12][3] = {"kk","lf","lg","ll","ls","nn","ns","pp","rr","ss","tt","
 int mc_freq[12] = {2,4,7,11,8,11,7,3,4,3,2,1};
 
 char fc_sound[107][4] = {"bdi","ch","che","chi","chs","ck","ct","d","da","dai",
-												 "dal","day","di","dro","dy","f","ft","g","gal","gh",
-												 "ghi","ght","hai","hdi","hi","hn","hr","j","k","ke",
-												 "ko","l","le","lf","li","lla","lle","ll","lt","ltz",
-												 "lly","lm","los","m","man","mm","mma","mms","mmy","mon",
-                				 "n","nas","ndi","ndy","ne","nes","nez","ng","ni","nk",
-                				 "nn","nne","nny","nos","ns","nt","nuz","p","ph","que",
-                				 "r","rc","rie","rk","rl","rly","rr","rt","rx","ry",
-                				 "s","sen","sha","shi","ski","son","ss","st","t","th",
-                				 "ti","tti","tts","tz","v","w","wdi","x","xx","yy",
-                				 "z","za","ze","zo","zy","zyx","zz"};
+						 "dal","day","di","dro","dy","f","ft","g","gal","gh",
+						 "ghi","ght","hai","hdi","hi","hn","hr","j","k","ke",
+						 "ko","l","le","lf","li","lla","lle","ll","lt","ltz",
+						 "lly","lm","los","m","man","mm","mma","mms","mmy","mon",
+						 "n","nas","ndi","ndy","ne","nes","nez","ng","ni","nk",
+						 "nn","nne","nny","nos","ns","nt","nuz","p","ph","que",
+						 "r","rc","rie","rk","rl","rly","rr","rt","rx","ry",
+						 "s","sen","sha","shi","ski","son","ss","st","t","th",
+						 "ti","tti","tts","tz","v","w","wdi","x","xx","yy",
+						 "z","za","ze","zo","zy","zyx","zz"};
 int fc_freq[107] = {3,3,1,5,2,5,2,8,5,2,7,3,5,7,5,4,4,9,3,5,2,3,2,2,4,
                			6,2,6,9,4,3,10,4,5,3,4,3,6,3,2,4,6,5,9,4,2,2,2,5,6,
                			10,4,7,5,2,7,7,5,7,9,5,3,3,7,5,7,3,7,5,2,9,5,3,8,7,
@@ -93,87 +93,87 @@ int looping = TRUE;
 //  START PROGRAM
 
 void _main(void)
-	{
+{
 		
 //	Make name
 
-		k = 0;
+	k = 0;
     for (i = 0; i < 45; i++)
         for (j = 0; j < ic_freq[i]; j++)
         {
-        		for (l = 0; l < 4; l++)
-        				ic_sounds[k][l] = ic_sound[i][l];
-        		printf("%s ",ic_sounds[k]);
+        	for (l = 0; l < 4; l++)
+        		ic_sounds[k][l] = ic_sound[i][l];
+        	printf("%s ",ic_sounds[k]);
             k++;
         }
-    //ngetchx();  
+//	ngetchx();  
     k = 0;
     for (i = 0; i < 8; i++)
         for (j = 0; j < v_freq[i]; j++)
         {
-        		for (l = 0; l < 4; l++)
-        				v_sounds[k][l] = v_sound[i][l];
-        		printf("%s ",v_sounds[k]);
+        	for (l = 0; l < 4; l++)
+        		v_sounds[k][l] = v_sound[i][l];
+        	printf("%s ",v_sounds[k]);
             k++;
         }
-		//ngetchx();
+//	ngetchx();
     k = 0;
     for (i = 0; i < 12; i++)
         for (j = 0; j < mc_freq[i]; j++)
         {
-        		for (l = 0; l < 4; l++)
-        				mc_sounds[k][l] = mc_sound[i][l];
-        		printf("%s ",mc_sounds[k]);
+        	for (l = 0; l < 4; l++)
+        		mc_sounds[k][l] = mc_sound[i][l];
+        	printf("%s ",mc_sounds[k]);
             k++;
         }
-		//ngetchx();
+//	ngetchx();
     k = 0;
     for (i = 0; i < 107; i++)
         for (j = 0; j < fc_freq[i]; j++)
         {
-        		for (l = 0; l < 5; l++)
-        				fc_sounds[k][l] = fc_sound[i][l];
-        		printf("%s ",fc_sounds[k]);
+        	for (l = 0; l < 5; l++)
+        		fc_sounds[k][l] = fc_sound[i][l];
+        	printf("%s ",fc_sounds[k]);
             k++;
         }
-    //ngetchx();
+//	ngetchx();
 
-		randomize();
+	randomize();
     
     while (looping){
     
-// roll characteristics
+//	roll characteristics
      
     for (i = 0; i < 6; i++)
         characteristic[i] = roll("2D6");
-//    characteristic[c_Soc] = 12;
+//	characteristic[c_Soc] = 12;
 
     proper = FALSE;
     while (!proper)
     {
-    		word[0] = 0;
+		word[0] = 0;
         temp = CC;
         while (temp == CC)
             temp = syllable_type[random(15)];
         if (temp == V)
-						strcat(word,v_sounds[random(37)]);
-    				//sound = v_sounds[random(114)][3];
-    		if (temp == CV)
-    				strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
-        		//sound = ic_sounds[random(789)] + v_sounds[random(114)];
-    		if (temp == VC)
-    				strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
-        		//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
-    		if (temp == CVC)
-    				strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
-        		//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
-    		if (temp == CC)
-    				strcat(word,mc_sounds[random(63)]);
-        		//sound = mc_sounds[random(176)];
+			strcat(word,v_sounds[random(37)]);
+    		//sound = v_sounds[random(114)][3];
+		if (temp == CV)
+			strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
+			//sound = ic_sounds[random(789)] + v_sounds[random(114)];
+		if (temp == VC)
+			strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
+			//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
+		if (temp == CVC)
+			strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
+			//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
+		if (temp == CC)
+			strcat(word,mc_sounds[random(63)]);
+			//sound = mc_sounds[random(176)];
         building = TRUE;
         while (building)
         {
-        	  syllable = syllable_type[random(15)];
+			syllable = syllable_type[random(15)];
             while (temp == CC && (syllable == CV || syllable == CVC || syllable == CC))
                 syllable = syllable_type[random(15)];
             while (temp == V && (syllable == V || syllable == VC))
@@ -188,32 +188,32 @@ void _main(void)
                 building = FALSE;
             else
             {
-            	  if (syllable == V)
-										strcat(word,v_sounds[random(37)]);
-    								//sound = v_sounds[random(114)][3];
-    						if (syllable == CV)
-    								strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
-        						//sound = ic_sounds[random(789)] + v_sounds[random(114)];
-    						if (syllable == VC)
-    								strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
-        						//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
-    						if (syllable == CVC)
-    								strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
-        						//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
-    						if (syllable == CC)
-    								strcat(word,mc_sounds[random(63)]);
-        						//sound = mc_sounds[random(176)];
+				if (syllable == V)
+					strcat(word,v_sounds[random(37)]);
+    				//sound = v_sounds[random(114)][3];
+				if (syllable == CV)
+						strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
+					//sound = ic_sounds[random(789)] + v_sounds[random(114)];
+				if (syllable == VC)
+						strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
+					//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
+				if (syllable == CVC)
+						strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
+					//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
+				if (syllable == CC)
+						strcat(word,mc_sounds[random(63)]);
+					//sound = mc_sounds[random(176)];
                 temp = syllable;
             }
         }        
         if (strlen(word) > 2 && strlen(word) < 10)
             proper = TRUE;
-		}
+	}
 		
-// 	clear the screen
-    clrscr();
+//	clear the screen
+	clrscr();
     
-// First name
+//	First name
 
     first_name[0] = 0;
     letter = word[0];
@@ -235,29 +235,29 @@ void _main(void)
     proper = FALSE;
     while (!proper)
     {
-    		word[0] = 0;
-    		temp = CC;
+    	word[0] = 0;
+    	temp = CC;
         while (temp == CC)
             temp = syllable_type[random(15)];
         if (temp == V)
-						strcat(word,v_sounds[random(37)]);
-    				//sound = v_sounds[random(114)][3];
-    		if (temp == CV)
-    				strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
-        		//sound = ic_sounds[random(789)] + v_sounds[random(114)];
-    		if (temp == VC)
-    				strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
-        		//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
-    		if (temp == CVC)
-    				strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
-        		//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
-    		if (temp == CC)
-    				strcat(word,mc_sounds[random(63)]);
-        		//sound = mc_sounds[random(176)];
+			strcat(word,v_sounds[random(37)]);
+    		//sound = v_sounds[random(114)][3];
+		if (temp == CV)
+			strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
+			//sound = ic_sounds[random(789)] + v_sounds[random(114)];
+		if (temp == VC)
+			strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
+			//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
+		if (temp == CVC)
+			strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
+			//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
+		if (temp == CC)
+			strcat(word,mc_sounds[random(63)]);
+			//sound = mc_sounds[random(176)];
         building = TRUE;
         while (building)
         {
-        	  syllable = syllable_type[random(15)];
+			syllable = syllable_type[random(15)];
             while (temp == CC && (syllable == CV || syllable == CVC || syllable == CC))
                 syllable = syllable_type[random(15)];
             while (temp == V && (syllable == V || syllable == VC))
@@ -272,31 +272,31 @@ void _main(void)
                 building = FALSE;
             else
             {
-            	  if (syllable == V)
-										strcat(word,v_sounds[random(37)]);
-    								//sound = v_sounds[random(114)][3];
-    						if (syllable == CV)
-    								strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
-        						//sound = ic_sounds[random(789)] + v_sounds[random(114)];
-    						if (syllable == VC)
-    								strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
-        						//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
-    						if (syllable == CVC)
-    								strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
-        						//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
-    						if (syllable == CC)
-    								strcat(word,mc_sounds[random(63)]);
-        						//sound = mc_sounds[random(176)];
+				if (syllable == V)
+					strcat(word,v_sounds[random(37)]);
+    				//sound = v_sounds[random(114)][3];
+				if (syllable == CV)
+						strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]);
+					//sound = ic_sounds[random(789)] + v_sounds[random(114)];
+				if (syllable == VC)
+						strcat(strcat(word,v_sounds[random(37)]),fc_sounds[random(484)]);
+					//sound = v_sounds[random(114)] + fc_sounds[random(1465)];
+				if (syllable == CVC)
+						strcat(strcat(strcat(word,ic_sounds[random(257)]),v_sounds[random(37)]),fc_sounds[random(484)]);
+					//sound = ic_sounds[random(789)] + v_sounds[random(114)] + fc_sounds[random(1465)];
+				if (syllable == CC)
+						strcat(word,mc_sounds[random(63)]);
+					//sound = mc_sounds[random(176)];
                 temp = syllable;
             }
         }
         if (strlen(word) > 2 && strlen(word) < 14)
             proper = TRUE;
-		}
+	}
 		
-// Last name
+//	Last name
 
-		last_name[0] = 0;
+	last_name[0] = 0;
     letter = word[0];
     letter = letter - 32;
     word[0] = letter;
@@ -305,7 +305,7 @@ void _main(void)
     
     printf("%s\n\n",last_name);
 
-// Print character
+//	Print character
 		
     printf("UPP: [");
     for (i = 0; i < 6; i++)
@@ -313,22 +313,23 @@ void _main(void)
     printf("]  Sex: %s\n\n", sex);
     
     for (i = 0; i < 3; i++)
-    		printf("%s:%2d  ", characteristic_name[i], characteristic[i]);
+    	printf("%s:%2d  ", characteristic_name[i], characteristic[i]);
     printf("\n");
     	
     for (i = 0; i < 3; i++)
-    		printf("Mod %2d  ", characteristic_dm[characteristic[i]]);
+    	printf("Mod %2d  ", characteristic_dm[characteristic[i]]);
     printf("\n\n");
     	
     for (i = 3; i < 6; i++)
-    		printf("%s:%2d  ", characteristic_name[i], characteristic[i]);
+    	printf("%s:%2d  ", characteristic_name[i], characteristic[i]);
     printf("\n");
     
     for (i = 3; i < 6; i++)
-    		printf("Mod %2d  ", characteristic_dm[characteristic[i]]);
+    	printf("Mod %2d  ", characteristic_dm[characteristic[i]]);
     
     ST_showHelp("Programmed by shawndriscoll@hotmail.com");
-    // wait for a key press before the program exits
+//	wait for a key press before the program exits
     if (ngetchx () == 13)
     	looping = FALSE;
-	}}
+	}
+}
